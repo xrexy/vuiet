7804dd27abbf49c710a4e73974d5673b78c9c6f4
xrexy <rexydoesthings@gmail.com>
2023-04-30 15:51:28 +0300

    delete unused .d.ts

--- src/components/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as WalletConnectButton } from "./WalletConnectButton.vue";
-export { default as WalletMultiButton } from "./WalletMultiButton.vue";
-export { default as WalletListItem } from "./WalletListItem.vue";
-export { default as ModalWrapper } from "./ModalWrapper.vue";
-export { default as SelectWalletModal } from "./modals/SelectWalletModal.vue";
-export { default as ConnectingWalletModal } from "./modals/ConnectingWalletModal.vue";
-export { default as InstallWalletModal } from "./modals/InstallWalletModal.vue";
