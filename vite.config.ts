7ac773955d70bc0bcca95413856010e6cf5e640f
xrexy <rexydoesthings@gmail.com>
2023-04-30 17:53:38 +0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,32 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'process.env': {}
+  },
   resolve: {
+    dedupe: ['vue', '@mysten/sui.js'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'vuiet'
+    },
+    rollupOptions: {
+      external: ['vue', '@vueuse/core', '@mysten/sui.js'],
+      output: {
+        exports: 'named',
+        globals: {
+          '@vueuse/core': 'VueUseCore',
+          '@mysten/sui.js': 'MystenSui',
+          vue: 'Vue'
+        }
+      }
+    },
+    sourcemap: true,
+    minify: false
   }
 })
