23e73876ee9ecf22823e4151b203c6671282de90
xrexy <rexydoesthings@gmail.com>
2023-05-01 18:29:28 +0300

    fix useAvailableWallets installed returning duplicates

--- src/composables/useAvailableWallets.ts
+++ src/composables/useAvailableWallets.ts
@@ -1,42 +1,40 @@
-import type { IWallet } from "../types";
-import { computed } from "vue";
-import { useWalletAdaptersDetection } from "./useWalletAdaptersDetection";
+import type { IWallet } from '../types'
+import { computed } from 'vue'
+import { useWalletAdaptersDetection } from './useWalletAdaptersDetection'
 
 export const useAvailableWallets = (def: IWallet[]) => {
-  const { availableAdapters } = useWalletAdaptersDetection();
+  const { availableAdapters } = useWalletAdaptersDetection()
 
   const configured = computed<IWallet[]>(() => {
-    if (def.length < 1) return [];
+    if (def.length < 1) return []
 
     if (availableAdapters.value.length == 0) {
       return def.map((defaultWallet) => ({
         ...defaultWallet,
         installed: false,
-        adapter: null,
-      }));
+        adapter: null
+      }))
     }
 
     return def.map((defaultWallet) => {
-      const adapter = availableAdapters.value.find(
-        (x) => x.name == defaultWallet.displayName
-      );
+      const adapter = availableAdapters.value.find((x) => x.name == defaultWallet.displayName)
 
       return adapter
         ? {
             ...defaultWallet,
             installed: true,
-            adapter,
+            adapter
           }
         : {
             ...defaultWallet,
             installed: false,
-            adapter: null,
-          };
-    });
-  });
+            adapter: null
+          }
+    })
+  })
 
   const detected = computed<IWallet[]>(() => {
-    if (availableAdapters.value.length == 0) return [];
+    if (availableAdapters.value.length == 0) return []
 
     return availableAdapters.value
       .filter((x) => def.find((w) => w.displayName === x.name))
@@ -44,26 +42,32 @@ export const useAvailableWallets = (def: IWallet[]) => {
         displayName: adapter.name,
         installed: true,
         icon: adapter.icon,
-        adapter,
-      }));
-  });
+        adapter
+      }))
+  })
 
   const configuredNonDetected = computed<IWallet[]>(() => {
-    if (availableAdapters.value.length == 0) return configured.value;
+    if (availableAdapters.value.length == 0) return configured.value
 
     return configured.value.filter(
       (w) => !detected.value.find((x) => x.displayName == w.displayName)
-    );
-  });
+    )
+  })
 
-  const installed = computed<IWallet[]>(() =>
-    [...configured.value, ...detected.value].filter((w) => w.installed)
-  );
+  const installed = computed<IWallet[]>(() => {
+    const withDuplicated = [...detected.value, ...configured.value]
+    const map = new Map<string, IWallet>()
+    for (const item of withDuplicated) {
+      map.set(item.displayName, item)
+    }
+
+    return Array.from(map.values())
+  })
 
   return {
     configured,
     installed,
     detected,
-    configuredNonDetected,
-  };
-};
+    configuredNonDetected
+  }
+}
